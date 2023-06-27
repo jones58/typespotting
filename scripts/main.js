7de81ef5735949a912e5ad51c5dd7895e9937a35
jones58 <108466184+jones58@users.noreply.github.com>
2023-06-27 18:56:23 +0100

    making javascript function for random 1st image

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,33 @@
 /* html selector */
 let htmlElement = document.getElementById("html");
 
-/* random color scheme */
+/* random color scheme out of 9 */
 function randomColorScheme() {
   let randomColorScheme = Math.floor(Math.random() * 10);
   htmlElement.classList.add("colorscheme" + randomColorScheme);
-  console.log("hello");
 }
 
 /* random colour scheme on launch */
 window.addEventListener("load", () => {
   randomColorScheme();
+  randomStartingImage();
 });
+
+/* random number within level 1 */
+let imageElement = document.querySelector("img");
+function randomStartingImage() {
+  let randomNumber = Math.floor(Math.random() * 7);
+  imageElement.setAttribute(
+    "src",
+    "images/level1/" + randomNumber + ".jpeg"
+  );
+  console.log(hello);
+}
 
 /* x y points in image - if click within set x y for particular image (1 of 20) then win. 
 /* when click Cooper Black (within set x y): 
-- highlight where cooper black is / the set x y coordinates. 
-- display : inline the right answer (#right-answer)
+- highlight where Cooper Black is / the set x y coordinates. 
+- display : inline the right answer text (#right-answer)
 - add 5 to score */
 
 /* when click wrong place/font: 
