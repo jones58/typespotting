7ef06feb70a96f9dc7fe1fde6106c7b882d0a1a0
jones58 <108466184+jones58@users.noreply.github.com>
2023-06-27 18:47:57 +0100

    writing pseudocode

--- scripts/main.js
+++ scripts/main.js
@@ -13,22 +13,21 @@ window.addEventListener("load", () => {
   randomColorScheme();
 });
 
-/* decide whether want buttons or x y points in image, probably the latter */
+/* x y points in image - if click within set x y for particular image (1 of 20) then win. 
+/* when click Cooper Black (within set x y): 
+- highlight where cooper black is / the set x y coordinates. 
+- display : inline the right answer (#right-answer)
+- add 5 to score */
+
+/* when click wrong place/font: 
+- highlight where cooper black is
+- display : inline the wrong answer text (#wrong-answer)
+- take 5 off score */
 
 /* display:none rules after first photo */
 
 /* change photo by changing src attribute, change buttons when this changes too. */
 
 /* start score at 300 and take 1 point off score per second using time intervals */
-
-/* when click Cooper Black 
-- highlight where cooper black is
-- display : inline the right answer (#right-answer)
-- add 5 to score */
-
-/* when click wrong font 
-- highlight where cooper black is
-- display : inline the wrong answer text (#wrong-answer)
-- take 5 off score */
 
 /*[] [ ] Use querySelector() and querySelectorAll() to gather HTML elements */
