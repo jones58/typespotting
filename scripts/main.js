e16887f994745f3b7a135921f7a07f37740eab1b
jones58 <108466184+jones58@users.noreply.github.com>
2023-07-07 11:47:06 +0100

    specifying js win lose functions for  levels

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ function randomColorScheme() {
 /* random colour scheme on launch */
 window.addEventListener("load", () => {
   randomColorScheme();
-  randomLevelOneImage();
+  levelOneImage();
 });
 
 /* selecting image on page */
@@ -18,7 +18,7 @@ let imageElement = document.querySelector("img");
 
 /* random image within level 1 */
 let levelOneNumbers = [1, 2, 3, 4, 5, 6];
-function randomLevelOneImage() {
+function levelOneImage() {
   let randomOneIndex = Math.floor(
     Math.random() * levelOneNumbers.length
   );
@@ -28,44 +28,42 @@ function randomLevelOneImage() {
     "src",
     "images/level1/" + randomOne + ".jpeg"
   );
-}
 
-/* x y points in image - if click within set x y for particular image (1 of 6) then win. */
+  if ((randomOne = 1)) {
+    /* if click within set x y, win() */
+    /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
+    randomLevelOneImage();
+  }
 
-if ((randomNumber1 = 1)) {
-  /* if click within set x y, win() */
-  /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
-  randomLevelOneImage();
-}
+  if ((randomOne = 2)) {
+    /* if click within set x y, win() */
+    /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
+  }
 
-if ((randomNumber1 = 2)) {
-  /* if click within set x y, win() */
-  /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
-}
+  if ((randomOne = 3)) {
+    /* if click within set x y, win() */
+    /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
+  }
 
-if ((randomNumber1 = 3)) {
-  /* if click within set x y, win() */
-  /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
-}
+  if ((randomOne = 4)) {
+    /* if click within set x y, win() */
+    /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
+  }
 
-if ((randomNumber1 = 4)) {
-  /* if click within set x y, win() */
-  /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
-}
+  if ((randomOne = 5)) {
+    /* if click within set x y, win() */
+    /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
+  }
 
-if ((randomNumber1 = 5)) {
-  /* if click within set x y, win() */
-  /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
-}
-
-if ((randomNumber1 = 6)) {
-  /* if click within set x y, win() */
-  /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
+  if ((randomOne = 6)) {
+    /* if click within set x y, win() */
+    /* if click outside set x y, lose() , - highlight where Cooper Black is / the set x y coordinates. */
+  }
 }
 
 /* random image within level 2 */
 let levelTwoNumbers = [1, 2, 3, 4, 5, 6];
-function randomLevelTwoImage() {
+function levelTwoImage() {
   let randomTwoIndex = Math.floor(
     Math.random() * levelTwoNumbers.length
   );
@@ -73,16 +71,21 @@ function randomLevelTwoImage() {
   levelTwoNumbers.splice(randomTwoIndex, 1);
   imageElement.setAttribute(
     "src",
-    "images/level1/" + randomTwo + ".jpeg"
+    "images/level2/" + randomTwo + ".jpeg"
   );
 }
 
 /* random image within level 3 */
-function randomLevelThreeImage() {
-  let randomNumber3 = Math.floor(Math.random() * 9);
+let levelThreeNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+function levelThreeImage() {
+  let randomThreeIndex = Math.floor(
+    Math.random() * levelThreeNumbers.length
+  );
+  let randomThree = levelThreeNumbers[randomThreeIndex];
+  levelThreeNumbers.splice(randomThreeIndex, 1);
   imageElement.setAttribute(
     "src",
-    "images/level3/" + randomNumber3 + ".jpeg"
+    "images/level3/" + randomThree + ".jpeg"
   );
 }
 
@@ -97,14 +100,16 @@ function timeScoreDown() {
 }
 setInterval(timeScoreDown, 1000);
 
-/* win function 
+/* win function for level 1 
 - display : inline the right answer text (#right-answer)
-- add 5 to score */
+- add 5 to score 
+- change image in level, or if played all six numbers, go to next level */
 
-/* lose function 
+function win() {}
+
+/* lose function  for level 1
 - display : inline the wrong answer text (#wrong-answer)
-- take 5 off score */
+- take 5 off score 
+- change image in level, or if played all six numbers, go to next level */
 
-/* display:none rules after first photo */
-
-/*[] [ ] Use querySelector() and querySelectorAll() to gather HTML elements */
+function lose() {}
